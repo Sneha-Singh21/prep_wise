e157f5c94c3ecec786a5763ba9af85011f18953b
Sneha <indsneha2002@gmail.com>
2025-08-08 15:49:34 +0530

    Project Completed

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDachGUQiyx2uzeGyD_HYUq2oH4QDuuxwY",
-    authDomain: "prepwise-810a4.firebaseapp.com",
-    projectId: "prepwise-810a4",
-    storageBucket: "prepwise-810a4.firebasestorage.app",
-    messagingSenderId: "151935645692",
-    appId: "1:151935645692:web:cbe6d2205d45c6a1dde479",
-    measurementId: "G-YQRCDG1JYN"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
